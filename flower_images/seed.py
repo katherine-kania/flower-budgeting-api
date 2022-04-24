5d21d375ca92f9c3b9304ff2d099b60dfe9f500f
katherine-kania <katherine.kania@gmail.com>
2022-04-24 19:36:17 -0400

    adds more seed flowers

--- flower_images/seed.py
+++ flower_images/seed.py
@@ -39,10 +39,10 @@ flowers = [
     Flower('carnation', 'violet', '', 1.25),
     Flower('carnation', 'black', '', 1.25),
 
-    Flower('chrysanthemum', 'white', '', 2.50),
-    Flower('chrysanthemum', 'pink', '', 2.50),
-    Flower('chrysanthemum', 'peach', '', 2.50),
-    Flower('chrysanthemum', 'yellow', '', 2.50),
+    Flower('chrysanthemum', 'white', '', 3),
+    Flower('chrysanthemum', 'pink', '', 3),
+    Flower('chrysanthemum', 'peach', '', 3),
+    Flower('chrysanthemum', 'yellow', '', 3),
 
     Flower('cosmos', 'white', '', 3),
     Flower('cosmos', 'black', '', 3),
@@ -59,11 +59,6 @@ flowers = [
     Flower('echinacea', 'yellow', '', 2),
     Flower('echinacea', 'red', '', 2),
 
-    Flower('garden roses', 'white', '', 10),
-    Flower('garden roses', 'pink', '', 10),
-    Flower('garden roses', 'peach', '', 10),
-    Flower('garden roses', 'red', '', 10),
-
     Flower('gypsophila (babies breath', 'white', '', 3),
 
     Flower('helleborus', 'white', '', 3),
@@ -74,8 +69,73 @@ flowers = [
     Flower('hydrangea', 'pink', '', 9),
     Flower('hydrangea', 'violet', '', 9),
 
-    Flower('hydrangea', 'violet', '', 10),
+    Flower('leucadendron', 'red', '', 3.50),
 
+    Flower('lilies, asiatic', 'white', '', 4),
+    Flower('lilies, asiatic', 'pink', '', 4),
+    Flower('lilies, asiatic', 'red', '', 4),
+    Flower('lilies, asiatic', 'yellow', '', 4),
+
+    Flower('lisianthus', 'white', '', 2.50),
+    Flower('lisianthus', 'pink', '', 2.50),
+    Flower('lisianthus', 'violet', '', 2.50),
+    Flower('lisianthus', 'red', '', 2.50),
+    Flower('lisianthus', 'peach', '', 2.50),
+
+    Flower('peony', 'white', '', 9),
+    Flower('peony', 'pink', '', 9),
+    Flower('peony', 'red', '', 9),
+    Flower('peony', 'white', '', 9),
+
+    Flower('poppy, royal wedding', 'white', '', 8),
+    Flower('poppy, pink ruffles', 'pink', '', 8),
+    Flower('poppy, purple', 'violet', '', 8),
+    Flower('poppy, orange', 'peach', '', 8),
+
+    Flower('protea, nerifolia', 'pink', '', 18),
+
+    Flower('protea, pincushion', 'red', '', 10),
+    Flower('protea, pincushion', 'yellow', '', 10),
+
+    Flower('queen anne\'s lace', 'white', '', 2.50),
+
+    Flower('ranunculus', 'white', '', 6),
+    Flower('ranunculus', 'pink', '', 6),
+    Flower('ranunculus', 'red', '', 6),
+    Flower('ranunculus', 'peach', '', 6),
+    Flower('ranunculus', 'violet', '', 6),
+    Flower('ranunculus', 'black', '', 6),
+
+    Flower('roses, garden', 'white', '', 10),
+    Flower('roses, garden', 'pink', '', 10),
+    Flower('roses, garden', 'peach', '', 10),
+    Flower('roses, garden', 'red', '', 10),
+
+    Flower('roses, spray', 'white', '', 3),
+    Flower('roses, spray', 'pink', '', 3),
+    Flower('roses, spray', 'peach', '', 3),
+    Flower('roses, spray', 'nude', '', 3),
+    Flower('roses, spray', 'red', '', 3),
+    Flower('roses, spray', 'violet', '', 3),
+
+    Flower('scabiosa', 'white', '', 2.50),
+    Flower('scabiosa', 'pink', '', 2.50),
+    Flower('scabiosa', 'violet', '', 2.50),
+    Flower('scabiosa', 'black', '', 2.50),
+
+    Flower('seaholly', 'violet', '', 1.50),
+
+    Flower('silver brunia', 'white', '', 3),
+
+    Flower('snapdragon', 'white', '', 5),
+    Flower('snapdragon', 'pink', '', 5),
+    Flower('snapdragon', 'red', '', 5),
+    Flower('snapdragon', 'yellow', '', 5),
+
+    Flower('stock', 'white', '', 4),
+    Flower('stock', 'pink', '', 4),
+    Flower('stock', 'violet', '', 4),
+    Flower('stock', 'peach', '', 4),
 
 
 ]
