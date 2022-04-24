4727924d2fdedba7d5bf1bd5d79be6c3e3615ca4
katherine-kania <katherine.kania@gmail.com>
2022-04-24 11:45:57 -0400

    Flower seed data started

---
+++ flower_images/seed.py
@@ -0,0 +1,43 @@
+#  'name': self.name,
+#  'color': self.color,
+#  'img,': self.img,
+#  'price_stem,': self.price_stem
+# from ..models import Flower
+
+flowers = [
+    Flower('allium', 'whites', '', 3),
+    Flower('allium', 'violets', '', 3),
+    Flower('allium', 'pink', '', 3),
+
+    Flower('andromedia', 'white', '', 4),
+
+    Flower('anemone', 'violet', '', 3.5),
+    Flower('anemone', 'white', '', 3.5),
+    Flower('anemone', 'red', '', 3.5),
+    Flower('anemone', 'pink', '', 3.5),
+
+    Flower('anthurium', 'red', '', 12),
+    Flower('anthurium', 'white', '', 12),
+    Flower('anthurium', 'peach', '', 12),
+
+    Flower('alstoemeria', 'white', '', 2),
+    Flower('alstoemeria', 'yellow', '', 2),
+    Flower('alstoemeria', 'pink', '', 2),
+    Flower('alstoemeria', 'red', '', 2),
+
+    Flower('astrantia', 'red', '', 2.75),
+
+    Flower('blushing bride', 'white', '', 5),
+
+    Flower('brodea', 'white', '', 2.5),
+    Flower('brodea', 'violet', '', 2.5),
+
+    Flower('carnation', 'white', '', 1.25),
+    Flower('carnation', 'nude', '', 1.25),
+    Flower('carnation', 'pink', '', 1.25),
+    Flower('carnation', 'red', '', 1.25),
+    Flower('carnation', 'violet', '', 1.25),
+    Flower('carnation', 'black', '', 1.25),
+
+
+]
