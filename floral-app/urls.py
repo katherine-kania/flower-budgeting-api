0411caf5534c9874b3b955eea2d9acdf9a2e60e9
katherine-kania <katherine.kania@gmail.com>
2022-06-23 10:33:02 -0400

    url update

--- floral-app/urls.py
+++ floral-app/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', [path.to.your.view]),
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
 ]
